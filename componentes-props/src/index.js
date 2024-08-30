d7c126b15ecce2adbdf12d4ec74f3a296a4d0ff1
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-30 16:00:36 -0300

    ap04(props): encerra a apostila

--- componentes-props/src/index.js
+++ componentes-props/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from  'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
 const App = () => {
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Ainda não chegou'
+  const funcaoOK = () => alert('Agradecemos a confirmação')
+  const funcaoNOK = () => alert('Verificaremos o ocorrido')
+  const componenteFeedback = 
+    <Feedback textoOK={textoOK} textoNOK={textoNOK} funcaoOK={funcaoOK} funcaoNOK={funcaoNOK}/>
   return (
     <div className='container border mt-2 py-5'>
       <div className='row'>
@@ -13,28 +21,34 @@ const App = () => {
       </div>
       <div className='row'>
         <div className='col-sm-12 col-lg-6 col-xl-4 border py-5'>
-          <Pedido 
-            data="22/04/2024"
-            icone="fa-solid fa-hard-drive fa-shake fa-3x"
-            titulo="SSD"
-            descricao="SSD Kingston"
-          />
+          <Cartao cabecalho="22/04/2024">
+            <Pedido 
+              icone="fa-solid fa-hard-drive fa-shake fa-3x"
+              titulo="SSD"
+              descricao="SSD Kingston"
+            />
+            {componenteFeedback}
+          </Cartao>
         </div>
         <div className='col-sm-12 col-lg-6 col-xl-4 border  py-5'>
-          <Pedido 
-            data="24/05/2024"
-            icone="fa-solid fa-computer-mouse fa-beat fa-3x"
-            titulo="Mouse"
-            descricao="Microsoft Mouse 3500"
-          />
+          <Cartao cabecalho="24/05/2024">
+            <Pedido 
+              icone="fa-solid fa-computer-mouse fa-beat fa-3x"
+              titulo="Mouse"
+              descricao="Microsoft Mouse 3500"
+            />
+            {componenteFeedback}
+          </Cartao>
         </div>
         <div className='col-sm-12 col-lg-12 col-xl-4 border  py-5'>
-          <Pedido 
-            data="25/05/2024"
-            icone="fa-solid fa-memory fa-fade fa-3x"
-            titulo="Memória RAM"
-            descricao="Memoria RAM de 16Gb"
-          />
+          <Cartao cabecalho="25/05/2024">
+            <Pedido 
+              icone="fa-solid fa-memory fa-fade fa-3x"
+              titulo="Memória RAM"
+              descricao="Memoria RAM de 16Gb"
+            />
+            {componenteFeedback}
+          </Cartao>
         </div>
       </div>
     </div>
