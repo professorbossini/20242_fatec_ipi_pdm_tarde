20b5f1c7279af951e4fddc908f219ae858bba2e4
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-30 18:08:42 -0300

    ap05(estacao-climatica): encerra o codigo 2.6.1
    - retomar definindo o objeto de icones

---
+++ estacao-climatica/src/index.js
@@ -0,0 +1,52 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icone: null
+    }
+  }
+
+  obterEstacao = (data, latitude) => {
+    const anoAtual = data.getFullYear()
+    //21/06
+    const d1 = new Date(anoAtual, 5,21)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //22/12
+    const d3 = new Date(anoAtual, 11, 22)
+    //21/03
+    const d4 = new Date(anoAtual, 2, 21)
+    const estouNoSul = latitude < 0
+    if(data >= d1 && data < d2)
+      return estouNoSul ? 'Inverno' : 'Verão'
+    if (data >= d2 && data < d3)
+      return estouNoSul ? 'Primavera' : 'Outono'
+    if (data >= d3 || data < d1)
+      return estouNoSul ? 'Verão' : 'Inverno'
+    return estouNoSul ? 'Outono' : 'Primavera'
+
+  }
+
+  render(){
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
+
+export default App
