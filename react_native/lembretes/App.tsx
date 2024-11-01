d57b36164939cf7b24d2433e44cb5a7d9a7f20ef
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-01 17:27:25 -0300

    ap02(react-native): encerra a apostila

--- react_native/lembretes/App.tsx
+++ react_native/lembretes/App.tsx
@@ -12,7 +12,7 @@ import {
 import { AntDesign } from '@expo/vector-icons'
 
 interface Lembrete {
-  id: string;
+  id?: string;
   texto: string;
 }
 
@@ -20,43 +20,84 @@ export default function App() {
   //guardar o que o usuário digita numa variável de estado, usando o hook useState, chame essa variável de lembrete e a função associada de setLembrete
   //exibir o lembrete logo abaixo do TextInput usando um Text
   //resultado esperado: a cada letra digitada, a aplicação exibe o novo texto logo abaixo do campo em que o usuário digita
-  const [lembrete, setLembrete] = useState('')
+  const [lembrete, setLembrete] = useState <Lembrete>({texto: ''})
   const [lembretes, setLembretes] = useState<Lembrete[]> ([])
+  const [emModoDeEdicao, setEmModoDeEdicao] = useState(false)
 
   const adicionar = () => {
-    if (lembrete.length >= 1){
+    if (lembrete.texto.length >= 1){
       //construir um lembrete com id igual à data atual do sistema e texto igual ao valor existente na variável de estado
       const novoLembrete: Lembrete = {
         id: Date.now().toString(),
-        texto: lembrete
+        texto: lembrete.texto
       }
       // utilizar o hook associado à lista de lembretes para adicionar o novo lembrete no final da lista
       setLembretes(lembretesAtual => [novoLembrete, ...lembretesAtual]) //spread
       //limpar o campo em que o usuário digita o lembrete atualizando a variável de estado corrrespondente
-      setLembrete('')
+      setLembrete({texto: ''})
     }
     else{
       Alert.alert('É preciso digitar um lembrete')
     }
 
   }
+
+  const remover = (lembrete: Lembrete) => {
+    // Alert.alert(
+    //   'Remover lembrete',
+    //   `Deseja remover este lembrete? ${lembrete.texto}`,
+    //   [
+    //     {
+    //       text: 'Cancelar',
+    //       style: 'cancel'
+    //     },
+    //     {
+    //       text: 'Remover',
+    //       style: 'destructive',
+    //       onPress: () => {
+    //         setLembretes(
+    //           lembretesAtual => lembretesAtual.filter(item => item.id !== lembrete.id)
+    //         )  
+    //       }
+    //     }
+    //   ]
+    // )
+    setLembretes(
+      lembretesAtual => lembretesAtual.filter(item => item.id !== lembrete.id)
+    )
+  }
+
+  const atualizar = () => {
+    const lembretesAtualizados = lembretes.map(item => {
+      if(item.id === lembrete.id)
+        return lembrete
+      return item
+    })
+    
+    setLembretes(lembretesAtualizados)
+    setEmModoDeEdicao(false)
+    setLembrete({texto: ''})
+  }
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
         placeholder='Digite um lembrete...'
-        onChangeText={(lembreteDigitado) => setLembrete(lembreteDigitado)}
-        value={lembrete}/>
+        onChangeText={(novoTexto) => setLembrete({id: lembrete.id, texto: novoTexto})}
+        value={lembrete.texto}/>
       <Pressable
         style={styles.button}>
         <Text
           style={styles.buttonText}
-          onPress={adicionar}>
-          Salvar lembrete
+          onPress={emModoDeEdicao ? atualizar : adicionar}>
+          {
+            emModoDeEdicao ? 'Atualizar lembrete' : 'Salvar lembrete'
+          }
         </Text>
       </Pressable>
       <FlatList
-        keyExtractor={item => item.id}
+        keyExtractor={item => (item.id)!}
         style={styles.list} 
         data={lembretes}
         renderItem={l => (
@@ -68,12 +109,16 @@ export default function App() {
             </Text>
             <View
               style={styles.listItemButtons}>
-              <Pressable>
+              <Pressable
+                onPress={() => remover(l.item)}>
                 <AntDesign 
                   name='delete'
                   size={24}/>
               </Pressable>
-              <Pressable>
+              <Pressable
+                onPress={() => {
+                  setLembrete({id: l.item.id, texto: l.item.texto})
+                  setEmModoDeEdicao(true)}}>
                 <AntDesign 
                   name='edit'
                   size={24}
